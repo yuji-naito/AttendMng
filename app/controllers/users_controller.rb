c13fa5be1b690eb3c1771e4a0042cf274d223736
yuji-naito <ec2-user@ip-172-31-32-109.us-east-2.compute.internal>
2018-08-20 14:31:31 +0000

    update to create and edit the account info.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,7 +66,7 @@ class UsersController < ApplicationController
   private
   
     def user_params
-      params.require(:user).permit(:name, :email, :password,
+      params.require(:user).permit(:name, :email, :belong, :password,
                                    :password_confirmation)
     end
     
---
+++ db/migrate/20180819044919_add_belong_to_users.rb
@@ -0,0 +1,5 @@
+class AddBelongToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :belong, :string
+  end
+end
